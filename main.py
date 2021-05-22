f0872de1778eb252143d6b82721457ed1a72bae1
Dom Aquino <dom.aquino@outlook.com>
2021-05-22 17:41:11 +0800

    Cleanup files

--- main.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter import messagebox
-
-window = Tk()
-
-messagebox.showinfo("Sample", "Info")
-
-window.mainloop()
--- palidrome_test.py
+++
@@ -1,4 +0,0 @@
-from project_euler import is_palindrome
-
-def test_is_palindrome_1():
-    assert True == is_palindrome(919)
